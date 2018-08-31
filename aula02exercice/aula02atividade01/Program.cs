eb6ecdb48a9635533da8d60ab722fe153a287513
guih2127 <guih2127@gmail.com>
2018-08-30 23:03:18 -0300

    aula 02 exercice

---
+++ aula02exercice/aula02atividade01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace aula02atividade01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            int[] numbers = new int[3];
+
+
+            Console.WriteLine("Digite o valor de X");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de Y");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de Z");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            numbers[0] = x;
+            numbers[1] = y;
+            numbers[2] = z;
+            
+            if (x > y && x > z)
+            {
+                Console.WriteLine("O maior número é X: " + x);
+            }
+            if (y > x & y > z)
+            {
+                Console.WriteLine("O maior número é Y: " + y);
+            }
+            if (z > x & z > y)
+            {
+                Console.WriteLine("O maior número é Z: " + z);
+            }
+
+            Array.Sort(numbers);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
