3d37e4003b17b23298537eb108e89596719dd482
guih2127 <guih2127@gmail.com>
2018-09-07 20:07:04 -0300

    list01exercice03

---
+++ lista01condicionais/exercicio03/exercicio03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior, menor, area;
+            Console.WriteLine("Vamos calcular a área de um losango, primeiro, digite a diagonal maior!");
+            maior = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Agora, digite a diagonal menor.");
+            menor = Convert.ToInt32(Console.ReadLine());
+
+            area = (maior * menor) / 2;
+
+            Console.WriteLine("O valor da área é " + area + "!");
+
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
