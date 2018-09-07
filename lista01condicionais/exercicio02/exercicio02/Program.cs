db55fa7a950886da61f70e48e475eaad2006bda1
guih2127 <guih2127@gmail.com>
2018-09-07 19:50:12 -0300

    list01exercice02

---
+++ lista01condicionais/exercicio02/exercicio02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Digite um número!");
+            x = Convert.ToInt32(Console.ReadLine());
+            x = -(x);
+
+            Console.WriteLine("Agora seu número é " + x + "!");
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+
+        }
+    }
+}
