535fb03aee6bcc68884bd939e3e7d75aa777fa4f
guih2127 <guih2127@gmail.com>
2018-09-07 20:29:38 -0300

    list01exercice04

---
+++ lista01condicionais/exercicio04/exercicio04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+
+            Console.WriteLine("Vamos fazer uma conversão, digite um número em graus Celsius!");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (celsius * 1.8) + 32;
+
+            Console.WriteLine("O seu número em Fahrenheit é " + fahrenheit + "!");
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
