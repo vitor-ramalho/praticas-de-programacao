663ede7d4492f6c2cbbc4f162a8b57e24552ead8
guih2127 <guih2127@gmail.com>
2018-09-07 20:46:17 -0300

    list01exercice05

---
+++ lista01condicionais/exercicio05/exercicio05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+
+            Console.WriteLine("Vamos fazer uns cálculos, digite o seu peso!");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Se você engordar 15%, voce terá " + (peso + (peso * 0.20)) + " quilos!");
+            Console.WriteLine("Se você emagrecer 20%, voce terá " + (peso - (peso * 0.15)) + " quilos!");
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+
+        }
+    }
+}
