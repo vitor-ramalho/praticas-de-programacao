9a53b6d7aae1e3dd6a126c1f6af21a5014b0dc47
guih2127 <guih2127@gmail.com>
2018-09-07 20:54:15 -0300

    list01exercice06

---
+++ lista01condicionais/exercicio06/exercicio06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minimo, salario;
+
+            Console.WriteLine("Digite o seu salário, vamos calcular quantos salários mínimos você ganha!");
+            salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora, digite o valor do salário mínimo atual.");
+            minimo = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você ganha " + (salario / minimo) + " salários mínimos.");
+
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
