55d08e1181e00d5ae9edb12a5ab3b5ad1a6582ff
guih2127 <guih2127@gmail.com>
2018-09-07 20:58:13 -0300

    list01exercice07

---
+++ lista01condicionais/exercicio07/exercicio07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade;
+
+            Console.WriteLine("Vamos calcular quantos dias você ja viveu, digite sua idade!");
+            idade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você viveu, aproximadamente, " + (idade * 365) + " dias!");
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
