a0b352e8c18244ae23bbeb2ce2be1c10880776b7
guih2127 <guih2127@gmail.com>
2018-09-13 22:30:22 -0300

    list01exercice12

---
+++ lista01condicionais/exercicio12/exercicio12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diarias;
+
+            Console.WriteLine("Digite o número de diárias que você deseja.");
+            diarias = Convert.ToInt32(Console.ReadLine());
+
+            if (diarias < 15)
+            {
+                Console.WriteLine("O total a ser pago é: R$" + (diarias * 1.50));
+            }
+            else if (diarias == 15)
+            {
+                Console.WriteLine("O total a ser pago é: R$" + (diarias));
+            }
+            else if (diarias > 15)
+            {
+                Console.WriteLine("O total a ser pago é: R$" + (diarias * 0.50));
+            }
+
+            Console.WriteLine("Aperte uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
