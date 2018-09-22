5e1b2f42886fe303d3a80e5e0534f17b7a806a7f
guih2127 <guih2127@gmail.com>
2018-09-22 17:47:15 -0300

    lista02exercicio04

---
+++ lista02repetição-while/exercicio04/exercicio04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacidade, peso, i;
+
+            capacidade = 0;
+            i = 0;
+
+            while(capacidade < 800)
+            {
+                Console.WriteLine("Digite o seu peso: ");
+                peso = Convert.ToInt32(Console.ReadLine());
+
+                capacidade = capacidade + peso;
+                i++;
+
+                Console.WriteLine("O peso total das pessoas presentes é de " + capacidade + "kgs.");
+                Console.WriteLine("O número de pessoas é de: " + i + " pessoas.");
+            }
+            Console.WriteLine("A capacidade de 800 quilos foi esgotada.");
+
+            Console.WriteLine("Pressione uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
