3847e75a6f626134bcc41b4ba254285370236ee8
guih2127 <guih2127@gmail.com>
2018-10-07 13:11:02 -0300

    list04exercice02

---
+++ lista04matrizes/exercicio02/exercicio02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Digite os números para a primeira coluna (4 números): ");
+                myArray[i, 0] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(myArray[i, 0]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
