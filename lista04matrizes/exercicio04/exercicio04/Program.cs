c75f1e0f0023112b6a83a5ed265ffb84b0c9b8d3
guih2127 <guih2127@gmail.com>
2018-10-07 17:22:51 -0300

    list04exercice04

---
+++ lista04matrizes/exercicio04/exercicio04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[3, 5];
+            int numerosSelec = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("Digite os números que deseja incluir na Array (três linhas, cinco números em cada): ");
+                    myArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if (myArray[i, j] >= 15 && myArray[i, j] <= 20)
+                    {
+                        numerosSelec++;
+                    }
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(myArray[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("A quantidade de números entre 15 e 20 dentro da Array é {0}", numerosSelec);
+            Console.ReadKey();
+        }
+    }
+}
