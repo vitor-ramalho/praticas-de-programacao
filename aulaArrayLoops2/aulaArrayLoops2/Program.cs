c9b1008a957462a8646929c7e59fdabf8a91e408
guih2127 <guih2127@gmail.com>
2018-10-07 11:51:28 -0300

    class about array and loops 2 and 3

---
+++ aulaArrayLoops2/aulaArrayLoops2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace aulaArrayLoops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = { "Bob", "Ted", "Tom", "Fred" };
+            // Primeiro, estamos criando uma Array básica de estudantes, composta por strings que são os nomes de cada um.
+            // Queremos um modo através do qual possamos descobrir se um nome específico de um aluno está dentro da Array.
+            int[] grades = { 77, 50, 78, 99 };
+            // Temos uma outra Array, que se refere às notas de cada aluno, ou seja, esse é o conceito de PARALLEL ARRAY.
+            // Arrays parelas são Arrays que se equivalem, elas tem a mesma length. Aqui, por exemplo, no index [0] temos "Bob" e sua nota 77.
+
+            Console.WriteLine("Enter student name to search for");
+            string studentsName = Console.ReadLine();
+            bool contains = false;
+            int studentGrade = 0;
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (studentsName == students[i])
+                {
+                    contains = true;
+                    studentGrade = grades[i];
+                    break;
+                }
+            }
+            // Com esse Loop, percorremos a Array e, caso o nome digitado seja igual à algum elemento da Array, contains se torna true.
+            // Além disso, criamos a variável studentGrade, que será igual ao número da posição [i] do Loop dentro da Array de Grades.
+
+            if (contains == true)
+            {
+                Console.WriteLine("O estudante está nessa classe.");
+                Console.WriteLine("A nota do estudante é igual a {0}", studentGrade);
+            }
+            else
+            {
+                Console.WriteLine("O estudante não está na classe.");
+            }
+            // Aqui fazemos uma condicional simples, se contains for true, retornamos que ele está na classe.
+
+            Console.WriteLine("Digite uma tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ aulaArrayLoops3/aulaArrayLoops3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace aulaArrayLoops3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Existem alguns metódos úteis para serem utilizados em Arrays.
+            // Na aula passada, para procurar um elemento na Array utilizamos uma SEQUENCIAL SEARCH, passando por cada elemento dela.
+            int[] myArray = {10, 20, 30, 40, 50 };
+
+            Console.WriteLine("Digite um número para ver se ele existe.");
+            int number = int.Parse(Console.ReadLine());
+            // Iremos utilizar uma BINARY SEARCH para ver se o elemento está na Array.
+            // Quando os números estão em ordem, uma BINARY SEARCH é o modo mais efetivo de procurar um elemento na Array.
+            // Esse tipo de procura estipula um midpoint e, dependendo da ordem, procura o número voltando ou avançando na Array.
+
+            int answer = Array.BinarySearch(myArray, number);
+            // Executamos a BINARY SEARCH na Array, passando dois argumentos necessários, a Array escolhida (myArray) e o que queremos encontrar (no caso a variável number)
+            if(answer < 0)
+            {
+                Console.WriteLine("O número não existe.");
+            }
+            else
+            {
+                Console.WriteLine("A posição do número na Array é {0}", answer);
+            }
+            // Awnser, caso seja um número encontrado, será sempre 0 ou maior, pois a posição de um número em uma Array será sempre 0 ou maior.
+            // Então, fazemos a condicional, caso answer < 0 ou não.
+
+            int[] myArray2 = { 10, 5, 7, 2, 55 };
+            // Quanto temos uma Array aleatória, como a Array acima, podemos ordená-la utilizando o metódo Sort.
+            Array.Sort(myArray2);
+            for (int i = 0; i < myArray2.Length; i++)
+            {
+                Console.WriteLine(myArray2[i]);
+            }
+
+            int[] myArray3 = { 10, 5, 7, 2, 55 };
+            // Com o metódo Reverse, conseguimos inverter a ordem de uma Array, ou seja, ela irá começar do 55.
+
+            Array.Reverse(myArray3);
+            for (int i = 0; i < myArray3.Length; i++)
+            {
+                Console.WriteLine(myArray3[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
