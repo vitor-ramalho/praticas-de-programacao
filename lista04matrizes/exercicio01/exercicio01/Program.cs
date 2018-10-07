79a6d4948be8ad317c6dbb57eabd1736c4696f04
guih2127 <guih2127@gmail.com>
2018-10-07 13:06:11 -0300

    list04exercice01

---
+++ lista04matrizes/exercicio01/exercicio01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Digite os números para a primeira linha (4 números): ");
+                myArray[0,i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(myArray[0, i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
