df04f836b57b1406f195547a084eae553b4ec901
guih2127 <guih2127@gmail.com>
2018-10-08 23:50:57 -0300

    list05exercice04

---
+++ lista05vetores/exercicio04/exercicio04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            int[] positivos = new int[10];
+            int[] negativos = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite números para colocar na Array (10 números):");
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+                if (myArray[i] < 0)
+                {
+                    negativos[i] = myArray[i];
+                }
+                else if (myArray[i] >= 0)
+                {
+                    positivos[i] = myArray[i];
+                }
+            }
+            Console.WriteLine("Vetor com todos os números: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(myArray[i] +  " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Vetor com os números positivos: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(positivos[i] + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Vetor com os números negativos: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(negativos[i] + " ");
+            }
+            Console.ReadKey();
+
+            //Perguntar
+        }
+    }
+}
