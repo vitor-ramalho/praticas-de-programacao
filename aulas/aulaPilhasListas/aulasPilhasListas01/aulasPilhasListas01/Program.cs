ed5b018798a7e3b7a0ce6e9b139a355b48b36798
guih2127 <guih2127@gmail.com>
2018-10-15 23:14:21 -0300

    add classes about stacks and queues

---
+++ aulas/aulaPilhasListas/aulasPilhasListas01/aulasPilhasListas01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aulasPilhasListas01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pilhaDeStrings = new Stack<string>();
+            pilhaDeStrings.Push("Brahma");
+            pilhaDeStrings.Push("Skol");
+            pilhaDeStrings.Push("Kaiser");
+
+            foreach (string cerveja in pilhaDeStrings)
+            {
+                Console.WriteLine(cerveja);
+            }
+            Console.WriteLine("");
+
+            pilhaDeStrings.Pop();
+
+            foreach (string cerveja in pilhaDeStrings)
+            {
+                Console.WriteLine(cerveja);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+// as PILHAS são como se fosse um 'empilhamento' dos dados. 
+// seguem o conceito do LIFO (last in first out), ou seja,
+// a primeira a entrar é a ultima a sair da pilha.
+// podemos ter uma pilha de qualquer coisa, como int, bool, string, etc.
+// utilizamos o metódo push para colocar algo na pilha, e pop para tirar.
+// O Pop nao leva nenhum argumento, ele SEMPRE retira o ultimo elemento adicionado.
+
---
+++ aulas/aulaPilhasListas/aulaspilhasListas02/aulaspilhasListas02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aulasPilhasListas01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> filaDoBanco = new Queue<string>();
+            filaDoBanco.Enqueue("Guilherme");
+            filaDoBanco.Enqueue("Matheus");
+            filaDoBanco.Enqueue("Victor");
+
+            foreach (string pessoa in filaDoBanco)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+            Console.WriteLine("");
+
+            filaDoBanco.Dequeue();
+
+            foreach(string pessoa in filaDoBanco)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+// FILAS (queues) seguem o conceito de FIRST IN FIRST OUT.
+// basicamente, o primeiro a entrar é o primeiro a sair,
+// ao contrário das STACKS, onde o primeiro a entrar,
+// é o ultimo a sair.
+// para adicionar elementos na fila, utilizamos o metódo ENQUEUE
+// para retirar elementos da pilha, utilizamos o metódo DEQUEUE
